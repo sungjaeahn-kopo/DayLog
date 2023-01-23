b221b360b621c82d8ee4fc406cfde3b025d2be04
sungjaeahn-kopo <dtw421@naver.com>
2023-01-24 02:26:37 +0900

    DL_Add WriteEditor

--- components/screens/WriteScreen.js
+++ components/screens/WriteScreen.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+import WriteEditor from '../WriteEditor';
 import WriteHeader from '../WriteHeader';
 
 function WriteScreen() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
   return (
     <SafeAreaView style={styles.block}>
-      <WriteHeader />
+      <KeyboardAvoidingView
+        style={styles.avoidingView}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <WriteHeader />
+        <WriteEditor
+          title={title}
+          body={body}
+          onChangeTitle={setTitle}
+          onChangeBody={setBody}
+        />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -14,6 +33,9 @@ const styles = StyleSheet.create({
   block: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  avoidingView: {
+    flex: 1,
   },
 });
 
