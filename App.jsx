ed4b415e7b8d123b6d866effd99eca392e45f200
sungjaeahn-kopo <dtw421@naver.com>
2023-01-25 23:05:37 +0900

    DL_Add SearchContext

--- App.jsx
+++ App.jsx
@@ -1,14 +1,17 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {LogContextProvider} from './components/contexts/LogContext';
+import {SearchContextProvider} from './components/contexts/SearchContext';
 import RootStack from './components/screens/RootStack';
 
 function App() {
   return (
     <NavigationContainer>
-      <LogContextProvider>
-        <RootStack />
-      </LogContextProvider>
+      <SearchContextProvider>
+        <LogContextProvider>
+          <RootStack />
+        </LogContextProvider>
+      </SearchContextProvider>
     </NavigationContainer>
   );
 }
--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -1,14 +1,46 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {
+  Pressable,
+  StyleSheet,
+  TextInput,
+  useWindowDimensions,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import SearchContext from './contexts/SearchContext';
 
 function SearchHeader() {
-  return <Text style={styles.block}>Hello</Text>;
+  const {width} = useWindowDimensions();
+  const {keyword, onChangeText} = useContext(SearchContext);
+  // 양옆 여백 16씩 => 32
+  return (
+    <View style={[styles.block, {width: width - 32}]}>
+      <TextInput
+        style={styles.input}
+        placeholder="검색어를 입력하세요."
+        value={keyword}
+        onChangeText={onChangeText}
+        autoFocus
+      />
+      <Pressable
+        style={({pressed}) => [styles.button, pressed && {opacity: 0.5}]}
+        onPress={() => onChangeText('')}>
+        <Icon name="cancel" size={20} color="#9e9e9e" />
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {
-    color: 'white',
-    backgroundColor: 'blue',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+  },
+  button: {
+    marginLeft: 8,
   },
 });
 
