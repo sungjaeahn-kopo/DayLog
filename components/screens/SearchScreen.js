fcf8ce9d80a7b03c790850b625260aadee00deda
sungjaeahn-kopo <dtw421@naver.com>
2023-01-26 00:28:06 +0900

    DL_Add EmptySearchResult

--- components/screens/SearchScreen.js
+++ components/screens/SearchScreen.js
@@ -1,18 +1,40 @@
 import React, {useContext} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import LogContext from '../contexts/LogContext';
 import SearchContext from '../contexts/SearchContext';
+import EmptySearchResult from '../EmptySearchResult';
+import FeedList from '../FeedList';
 
 function SearchScreen({navigation}) {
   const {keyword} = useContext(SearchContext);
+  const {logs} = useContext(LogContext);
+
+  const filtered =
+    keyword === ''
+      ? []
+      : logs.filter(log =>
+          [log.title, log.body].some(text => text.includes(keyword)),
+        );
+
+  if (keyword === '') {
+    return <EmptySearchResult type="EMPTY_KEYWORD" />;
+  }
+
+  if (filtered.length === 0) {
+    return <EmptySearchResult type="NOT_FOUND" />;
+  }
+
   return (
     <View style={styles.block}>
-      <Text>{keyword}</Text>
+      <FeedList logs={filtered} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {
+    flex: 1,
+  },
 });
 
 export default SearchScreen;
